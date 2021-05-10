34c638696d64c30b9668073fc63d084cdcb4a145
bram de looze <bramdump@hotmail.com>
2021-05-10 11:15:48 +0200

    Made nearly all assets + player controller

---
+++ Afstudeerproject 2/Assets/Scripts/PleyerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PleyerMovement : MonoBehaviour
+{
+    //private Rigidbody myRigidBody;
+    public float movementSpeed = 5;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float verticalInput = Input.GetAxis("Vertical") * movementSpeed;
+        float horizontalInput = Input.GetAxis("Horizontal") * movementSpeed;
+        verticalInput *= Time.deltaTime;
+        horizontalInput *= Time.deltaTime;
+
+        transform.Translate(horizontalInput, 0, verticalInput);
+        
+        //update the position
+        //transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0, verticalInput * movementSpeed * Time.deltaTime);// x,y,z
+    }
+}
