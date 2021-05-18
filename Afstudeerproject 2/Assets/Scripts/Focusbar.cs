c8961f7e0c7f56d7c0309c3273894793d1f8f851
bram de looze <bramdump@hotmail.com>
2021-05-18 11:20:14 +0200

    Changed productivitybar.cs to Focusbar.cs Added focusSlider in script

---
+++ Afstudeerproject 2/Assets/Scripts/Focusbar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Focusbar : MonoBehaviour
+{
+    private Slider focusSlider;
+
+    private void Awake()
+    {
+        focusSlider = GetComponent<Slider>();
+    }
+
+    public void MoveSliderUp(float value)
+    {
+        focusSlider.value += value;
+    }
+}
