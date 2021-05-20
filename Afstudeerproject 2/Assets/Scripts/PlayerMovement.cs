9281449f5cf4bd1a901224a79e699b5e58298a4f
bram de looze <bramdump@hotmail.com>
2021-05-20 11:24:41 +0200

    Changed movementSpeed to VerticalMovementSpeed

--- Afstudeerproject 2/Assets/Scripts/PlayerMovement.cs
+++ Afstudeerproject 2/Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,31 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //private Rigidbody myRigidBody;
-    public float movementSpeed = 5;
+    public float VerticalMovementSpeed = 5;
 
     public static event Action OnPlayerFlying;
+    /*
+    private void OnEnable()
+    {
+        FocusBar.OnFocusTooLow += FocusTooLow;
+        FocusBar.OnFocusNormal += FocusNormal;
+    }
+    private void OnDisable()
+    {
+        FocusBar.OnFocusTooLow -= FocusTooLow;
+        FocusBar.OnFocusNormal -= FocusNormal;
+    }
+
+    void FocusTooLow()
+    {
+        ChangeMovementSpeed(VerticalMovementSpeed / 5);
+    }
+
+    void FocusNormal()
+    {
+        ChangeMovementSpeed(VerticalMovementSpeed);
+    }
+    */
 
     // Update is called once per frame
     void FixedUpdate()
@@ -27,10 +49,15 @@ public class PlayerMovement : MonoBehaviour
     //Moves the player on the y-axis but within the borders of the camera
     private void Move()
     {
-        float verticalInput = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        float verticalInput = Input.GetAxis("Vertical") * VerticalMovementSpeed * Time.deltaTime;
 
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4.5f, 4.5f), transform.position.z);
 
         transform.Translate(0, verticalInput, 0);
+    }
+
+    public void ChangeMovementSpeed(float speed)
+    {
+        VerticalMovementSpeed = speed;
     }
 }
