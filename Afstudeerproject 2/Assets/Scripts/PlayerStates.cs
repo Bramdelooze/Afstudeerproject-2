76cdc5c24c3278e14d71f2958668322587e82b78
bram de looze <bramdump@hotmail.com>
2021-05-20 15:39:43 +0200

    Added assets

--- Afstudeerproject 2/Assets/Scripts/PlayerStates.cs
+++ Afstudeerproject 2/Assets/Scripts/PlayerStates.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerStates : MonoBehaviour
 {
     public float movementSpeed;
     private FocusBar focusBar;
+    private bool enemyIsInRange = false;
+    private GameObject damagedEnemy;
 
     private void Awake()
     {
@@ -25,10 +28,44 @@ public class PlayerStates : MonoBehaviour
 
     private void Attack()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) //&& the sword hits an enemy with the animation
+        if (Input.GetKeyDown(KeyCode.Space) && enemyIsInRange)
         {
-            focusBar.MoveSlider(10);
+            KillEnemy();
+            focusBar.MoveSlider(20);
         }
+    }
+
+    private void KillEnemy()
+    {
+        if(damagedEnemy != null)
+            Destroy(damagedEnemy);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (IsCollisionEnemy(collision))
+            enemyIsInRange = true;
+
+        if(collision.tag == "UFO" && Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(IsCollisionEnemy(collision))
+            enemyIsInRange = false;
+    }
+
+    private bool IsCollisionEnemy(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            damagedEnemy = collision.gameObject;
+            return true;
+        }
+        else return false;
     }
 
     private void Move()
