e62901d3126174ecb1088964393ea6221b542a4a
bram de looze <bramdump@hotmail.com>
2021-05-20 11:25:01 +0200

    Added a distance tracker

---
+++ Afstudeerproject 2/Assets/Scripts/TravelDistanceTracker.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class TravelDistanceTracker : MonoBehaviour
+{
+    private TMP_Text distanceText;
+    [SerializeField] private float maxDistance = 1000;
+    [SerializeField] private float normalTravelSpeed = 10;
+    [SerializeField] private float slowTravelSpeed = 1;
+    private float currentDistanceTraveled;
+    private float currentTravelSpeed;
+
+    private void OnEnable()
+    {
+        PlayerMovement.OnPlayerFlying += PlayerIsFlying;
+        FocusBar.OnFocusNormal += MoveTravelSpeedToNormal;
+        FocusBar.OnFocusTooLow += MoveTravelSpeedToSlow;
+    }
+    private void OnDisable()
+    {
+        PlayerMovement.OnPlayerFlying -= PlayerIsFlying;
+        FocusBar.OnFocusNormal -= MoveTravelSpeedToNormal;
+        FocusBar.OnFocusTooLow -= MoveTravelSpeedToSlow;
+    }
+
+    private void Awake()
+    {
+        distanceText = GetComponent<TMP_Text>();
+        currentTravelSpeed = normalTravelSpeed;
+    }
+
+    void PlayerIsFlying()
+    {
+        MoveDistanceUp(currentTravelSpeed);
+    }
+
+    void MoveTravelSpeedToSlow()
+    {
+        currentTravelSpeed = slowTravelSpeed;
+    }
+
+    void MoveTravelSpeedToNormal()
+    {
+        currentTravelSpeed = normalTravelSpeed;
+    }
+
+    void MoveDistanceUp(float value)
+    {
+        currentDistanceTraveled += value * Time.deltaTime;
+        ChangeDistanceText();
+    }
+
+    private void ChangeDistanceText()
+    {
+        distanceText.text = currentDistanceTraveled.ToString("N0") + "m / " + maxDistance + "m"; 
+    }
+}
